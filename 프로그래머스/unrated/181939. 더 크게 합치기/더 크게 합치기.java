9569888d1a75d0dcc37d066e32ef65618fa0d394
Mozz-Aic <79677324+Mozz-Aic@users.noreply.github.com>
2023-05-08 20:36:39 +0900

    [unrated] Title: 더 크게 합치기, Time: 2.19 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int case1 = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+        int case2 = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        if(case1 >= case2) {
+            answer = case1;
+        } else {
+            answer = case2;
+        } 
+        return answer;
+    }
+}
